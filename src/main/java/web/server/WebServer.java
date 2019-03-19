209634ef92d7a9c4745822cabb74847b19d00fac
ben-williamson-1999 <ben.williamson1@ntlworld.com>
2019-03-19 11:43:17 +0000

    Initial commit

---
+++ src/main/java/web/server/WebServer.java
@@ -0,0 +1,32 @@
+package web.server;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.*;
+
+public class WebServer {
+
+    public static void main(String[] args){
+
+        System.setProperty("com.spark.shaded.eclipse.jetty.LEVEL","OFF");
+
+        port(8080);
+
+        post("/posttest", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String name = request.queryParams("name");
+                String colour = request.queryParams("favorite");
+
+                String returnHtml = "<html><body><p style=\"color:"+ colour + ";\">Hello, " + name + "</p></body></html>";
+                return returnHtml;
+            }
+
+        });
+
+        notFound("404 Not Found");
+
+    }
+}
